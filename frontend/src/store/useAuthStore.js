46ce23955a66586f2ac17c25340afacf21025f74
AnjuuYadav <anjuuyadav05@gmail.com>
2025-10-25 08:33:31 +0530

    some change in frontend to deploy

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 
-const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:3000": "/";
+const BASE_URL = import.meta.env.MODE === "development"? "http://localhost:3000": " https://fullstack-chat-app-backend-z7v9.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
